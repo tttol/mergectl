8991161332c36e57fcc41570c4e67f479a6578c1
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-07-12 07:59:24 +0900

    add cobra

---
+++ cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "mergectl",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello mergectl")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/ttto/mergectl/cmd"
+
+func main() {
+	cmd.Execute()
+}
