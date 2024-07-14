494a94c46b560cbc091c552a0ba572cb1b778636
TOL <tttol.t4k.b1z@gmail.com>
2024-07-14 16:32:13 +0900

    add ldflags (#5)

--- cmd/version.go
+++ cmd/version.go
@@ -6,11 +6,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var version string
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of mergectl",
 	Run: func(cmd *cobra.Command, args []string) {
-		version := "1.0.0"
 		fmt.Println("mergectl version:", version)
 	},
 }
